a7f442229d402b89d22c43fbecaa2b670e57477d
Achel <rachel1665@iCloud.com>
2022-07-21 00:15:00 +0800

    可以点击卡ç访问网页

--- dist/index.de158e3a.js
+++ dist/index.de158e3a.js
@@ -1,7 +1,54 @@
-$(".addButton").on("click", ()=>{
-    let url = window.prompt("\u8BF7\u8F93\u5165\u8981\u6DFB\u52A0\u7684\u7F51\u5740");
-    if (url.indexOf("http") !== 0) url = "https://" + url;
-    console.log(url);
-});
+const $siteList = $(".siteList");
+const $lastLi = $siteList.find("li.last");
+const urlName = localStorage.getItem("urlName");
+const urlNameObject = JSON.parse(urlName);
+const hashMap = urlNameObject || [
+    {
+        logo: "G",
+        url: "https://google.com"
+    },
+    {
+        logo: "C",
+        url: "https://caniuse.com"
+    },
+    {
+        logo: "M",
+        url: "https://developer.mozilla.org"
+    },
+    {
+        logo: "W",
+        url: "https://wandoc.com"
+    },
+    {
+        logo: "Z",
+        url: "https://zhangxinxu.com"
+    }
+];
+const simplifyUrl = (url)=>{
+    return url.replace("https://", "").replace("http://", "").replace("www.", "").replace(/\/.*/, "");
+};
+const render = ()=>{
+    $siteList.find("li:not(.last)").remove();
+    hashMap.forEach((node, index)=>{
+        const $li = $(`<li>
+                <div class="site">
+                    <div class="logo">${node.logo}</div>
+                    <div class="link">${simplifyUrl(node.url)}</div>
+                    <div class="close">
+                        <img class="closeImg" src="images/close.png" alt="close">
+                    </div>
+                </div>
+            </li>`).insertBefore($lastLi);
+        $li.on("click", ()=>{
+            window.open(node.url);
+        });
+        $li.on("click", ".close", (event)=>{
+            event.stopPropagation();
+            hashMap.splice(index, 1);
+            render();
+        });
+    });
+};
+render();
 
 //# sourceMappingURL=index.de158e3a.js.map
--- src/main.js
+++ src/main.js
@@ -1,8 +1,43 @@
-$('.addButton')
-    .on('click', () => {
-        let url = window.prompt('请输入要添加的网址')
-        if(url.indexOf('http') !== 0) {
-            url = 'https://' + url
-        }
-        console.log(url)
+const $siteList = $('.siteList')
+const $lastLi = $siteList.find('li.last')
+const urlName = localStorage.getItem('urlName')
+const urlNameObject = JSON.parse(urlName)
+const hashMap = urlNameObject || [    //用hash map保存初始的网站和后续添加的网站
+    {logo: 'G', url: 'https://google.com'},
+    {logo: 'C', url: 'https://caniuse.com'},
+    {logo: 'M', url: 'https://developer.mozilla.org'},
+    {logo: 'W', url: 'https://wandoc.com'},
+    {logo: 'Z', url: 'https://zhangxinxu.com'}
+]
+const simplifyUrl = (url) => {    //格式化要显示的网址
+    return url.replace('https://', '')
+        .replace('http://', '')
+        .replace('www.', '')
+        .replace(/\/.*/, '')
+}
+
+const render = () => {
+    $siteList.find('li:not(.last)').remove()
+    hashMap.forEach((node, index) => {
+        const $li = $(`<li>
+                <div class="site">
+                    <div class="logo">${node.logo}</div>
+                    <div class="link">${simplifyUrl(node.url)}</div>
+                    <div class="close">
+                        <img class="closeImg" src="images/close.png" alt="close">
+                    </div>
+                </div>
+            </li>`).insertBefore($lastLi)
+        $li.on('click', () => {
+            window.open(node.url)
+        })
+        $li.on('click', '.close', (event) => {
+            event.stopPropagation()
+            hashMap.splice(index, 1)
+            render()
+        })
     })
+}
+
+render()
+
