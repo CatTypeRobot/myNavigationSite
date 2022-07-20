e45e0c6cbb8ccaf3746b1b6723650b31c322985f
Achel <rachel1665@iCloud.com>
2022-07-20 17:28:14 +0800

    开始写js

--- dist/index.de158e3a.js
+++ dist/index.de158e3a.js
@@ -1,3 +1,7 @@
-console.log("hello world");
+$(".addButton").on("click", ()=>{
+    let url = window.prompt("\u8BF7\u8F93\u5165\u8981\u6DFB\u52A0\u7684\u7F51\u5740");
+    if (url.indexOf("http") !== 0) url = "https://" + url;
+    console.log(url);
+});
 
 //# sourceMappingURL=index.de158e3a.js.map
--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-console.log('hello world')
+$('.addButton')
+    .on('click', () => {
+        let url = window.prompt('请输入要添加的网址')
+        if(url.indexOf('http') !== 0) {
+            url = 'https://' + url
+        }
+        console.log(url)
+    })
